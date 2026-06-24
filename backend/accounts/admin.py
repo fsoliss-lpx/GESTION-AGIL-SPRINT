23567fe4918c4b6777c448efad71594fdf4087c8
fsoliss-lpx <fsoliss@unemi.edu.ec>
2026-06-24 04:19:49 -0500

    Registrar modelo admin y levantar servidor prueba

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Usuario
 
-# Register your models here.
+# Registramos nuestro usuario personalizado usando la vista nativa de Django
+admin.site.register(Usuario, UserAdmin)
--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Sprint, Tarea
 
-# Register your models here.
+admin.site.register(Sprint)
+admin.site.register(Tarea)
